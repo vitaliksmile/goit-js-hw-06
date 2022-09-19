bae54c570e5a354273c130c4b716c568b7e355dd
vitaliksmile <vitaliksmile777@gmail.com>
2022-09-19 20:37:07 +0300

    task-2-done

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listRef = document.querySelector("#ingredients");
+ingredients.forEach((item) => {
+  const liEl = document.createElement("li");
+  liEl.classList.add("item");
+  liEl.textContent = item;
+  listRef.append(liEl);
+  // console.log(liEl);
+  return liEl;
+});
+console.log(listRef);
