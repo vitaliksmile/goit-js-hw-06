13eee780d4a9087f044edbb5d484b501ac10fa2c
vitaliksmile <vitaliksmile777@gmail.com>
2022-09-20 22:04:10 +0300

    task-08-done

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,16 @@
-
+const form = document.querySelector(".login-form");
+let userList = [];
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    alert("You did not fill all input fields");
+  } else {
+    userList.email = email.value;
+    userList.password = password.value;
+    console.log(userList);
+    form.reset();
+  }
+});
