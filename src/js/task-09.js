f9eb030ca143001e9b9a9706cbca5a3402aff5cd
vitaliksmile <vitaliksmile777@gmail.com>
2022-09-20 22:25:45 +0300

    task-09-done

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,16 @@
+const refs = {
+  bodyEl: document.querySelector("body"),
+  textEl: document.querySelector(".color"),
+  buttonEl: document.querySelector(".change-color"),
+};
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+function ChangeColor() {
+  refs.bodyEl.style.backgroundColor = getRandomHexColor();
+  refs.textEl.textContent = `${getRandomHexColor()}`;
+}
+
+refs.buttonEl.addEventListener("click", ChangeColor);
